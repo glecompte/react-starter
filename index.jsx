36d7b93c87b1e861fc60f5d4ac87dbe9ef1cbfd4
Garret LeCompte <glecompte@gmail.com>
2016-03-28 15:35:57 -0700

    added stage 0

--- index.jsx
+++ index.jsx
@@ -1,13 +1,16 @@
 require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 export class App extends React.Component {
+	constructor(props) {
+		super(props)
+	}
 	render() {
-		return (
-			<div>Simple React + Babel + Bootstrap + Webpack</div>
-		);
+		return ( <div>
+			</div>
+		)
 	}
 }
 
-ReactDOM.render(<App/>, document.querySelector("#myApp"));
+ReactDOM.render(<App/>, document.querySelector("#myApp"))
